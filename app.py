a1aedfd695c4b037d5689569c4c7ec3cd1c45024
anubhav13@gmail.com <Evolution2019!>
2019-06-15 20:44:38 -0700

    Initial Commit

--- app.py
+++ app.py
@@ -4,9 +4,10 @@ import numpy as np
 import requests
 
 #GLOBAL VARIABLES
+DEV_HOST = 'http://localhost:8501/v1/models/mercury:predict'
+PROD_HOST = 'http://tf-serving-server:8501/v1/models/mercury:predict'
 
 app = Flask(__name__)
-
 
 @app.route('/')
 def hello_world():
@@ -16,15 +17,12 @@ def hello_world():
 def load():
     toPredict = np.loadtxt(".//test_data//predict_from_server.csv", delimiter=",")
     toPredict_list = toPredict.tolist()
-    # df = load_data(data_path)
-    # targets_ohe = process_data(df)
-    # x_pred = normalize_data(df)
+
 
     payload={
         "instances":[{'dense_input':toPredict_list}]
     }
-    response = requests.post('http://localhost:8501/v1/models/mercury:predict', json=payload)
-    # response = requests.post('http://tf-serving-server:8501/v1/models/mercury:predict',json = payload)
+    response = requests.post(PROD_HOST, json=payload)
     predictions = response.json()['predictions'][0]
     return jsonify(predictions)
 
